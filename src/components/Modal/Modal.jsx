df80541daf7ac7c4eb328126800d133fa71e7826
Zhukov Vitaliy <felex180590@gmail.com>
2023-02-24 12:11:07 +0200

    refactoring

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import { useEffect, Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Overlay, ModalStyled } from './Modal.styled';
 
@@ -43,38 +43,3 @@ export const Modal = ({ onClose, children, largeImage }) => {
     modalRoot
   );
 };
-
-export class OldModal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handeleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handeleKeyDown);
-  }
-
-  handeleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  hendleBackdropClick = e => {
-    if (e.currentTarget === e.target) {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-    const { largeImage } = this.props;
-    return createPortal(
-      <Overlay onClick={this.hendleBackdropClick}>
-        <ModalStyled>
-          <img src={largeImage} alt="large version" />
-          {this.props.children}
-        </ModalStyled>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
