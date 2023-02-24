db21a778da373ae5f334f82dd90947fb9f22f6f4
Zhukov Vitaliy <felex180590@gmail.com>
2023-02-24 12:06:10 +0200

    executes the task hooks-hw-04-ImageGallery

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, Component } from 'react';
 import { Container } from './App.styled';
 import { Button } from './Button/Button';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -7,7 +7,78 @@ import { Searchbar } from './Searchbar/Searchbar';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 
-export class App extends Component {
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [status, setStatus] = useState('idle');
+  const [showModal, setShowModal] = useState(false);
+  const [largeImage, setLargeImage] = useState(null);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      return;
+    }
+
+    setStatus('pending');
+
+    getFetch(searchQuery, page)
+      .then(({ hits }) => {
+        setImages(images => [...images, ...hits]);
+        setStatus('resolved');
+      })
+      .catch(setStatus('rejected'));
+  }, [page, searchQuery]);
+
+  const addSearchQuery = searchQuery => {
+    setImages([]);
+    setPage(1);
+    setSearchQuery(searchQuery);
+  };
+
+  const toggleModal = () => {
+    setShowModal(showModal => !showModal);
+  };
+
+  const scroll = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth',
+    });
+  };
+
+  const loadMore = () => {
+    setPage(page => page + 1);
+    scroll();
+  };
+
+  const showLoadMore = images.length > 0;
+  const pending = status === 'pending';
+
+  return (
+    <>
+      <Container>
+        <Searchbar onSubmit={addSearchQuery} />
+
+        <ImageGallery
+          images={images}
+          onShowModal={toggleModal}
+          onLargeImage={setLargeImage}
+        />
+
+        {showModal && (
+          <Modal onClose={toggleModal} largeImage={largeImage}></Modal>
+        )}
+
+        {pending && <Loader />}
+
+        {showLoadMore && <Button onLoadMore={loadMore} />}
+      </Container>
+    </>
+  );
+};
+
+export class OldApp extends Component {
   state = {
     searchQuery: '',
     images: [],
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,50 @@
-import { Component } from 'react';
+import { useEffect, Component } from 'react';
 import { createPortal } from 'react-dom';
 import { Overlay, ModalStyled } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
+export const Modal = ({ onClose, children, largeImage }) => {
+  // useEffect(() => {
+  //   const handleKeyDown = e => console.log('keydown event: ', e);
+  //   document.addEventListener('keydown', handleKeyDown);
+  //   return () => {
+  //     document.removeEventListener('keydown', handleKeyDown);
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    window.addEventListener('keydown', handeleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handeleKeyDown);
+    };
+  });
+
+  const handeleKeyDown = e => {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  };
+
+  const hendleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+
+  return createPortal(
+    <Overlay onClick={hendleBackdropClick}>
+      <ModalStyled>
+        <img src={largeImage} alt="large version" />
+        {children}
+      </ModalStyled>
+    </Overlay>,
+    modalRoot
+  );
+};
+
+export class OldModal extends Component {
   componentDidMount() {
     window.addEventListener('keydown', this.handeleKeyDown);
   }
